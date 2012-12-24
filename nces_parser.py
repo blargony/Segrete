f5b606330027bf5c670707400d4add3f29436066
Robert Chapman <blargony@users.noreply.github.com>
2012-12-23 16:57:01 -0800

    Enhanced ot handle varied format file format that came up around '98. Cleaned up a few other elements including adding a method to convert a dataset to a dictionary index by the headers.

--- nces_parser.py
+++ nces_parser.py
@@ -5,6 +5,7 @@ Parse the NCES Data File Format Doc and create parsing instructions for the main
 
 import argparse
 import re
+import os
 import csv
 
 # Unit testing
@@ -16,6 +17,12 @@ import unittest
 # resistor_name ([node_name] [node_name]) resistor r=[resistance_value] c=0
 re_definition = re.compile(r'^(\w+)\s+(\w+)\s+(\d+)[-](\d+)\s+(\d+)*?\s+(.*)$')
 re_sub_definition = re.compile(r'^\s*[+](\w+)\s+(\w+)\s+(\d+)[-](\d+)\s+(\d+)\s+(.*)$')
+re_alt_definition = re.compile(r'^(\w+)\s+(\d+)\s+(\d+)\s+(\d+)*?\s+(\w+)\s+(.*)$')
+re_alt_sub_definition = re.compile(r'^\s*[+](\w+)\s+(\d+)\s+(\d+)\s+(\d+)*?\s+(\w+)\s+(.*)$')
+
+
+datafile_name = "nces%02d-%02d.txt"
+formatfile_name = "nces%02d-%02d_layout.txt"
 
 # ==============================================================================
 # Utility Functions
@@ -59,21 +66,46 @@ class NCESParser(object):
         [('COLUMN_NAME', idx), ('COLUMN_NAME', idx), ...]
 
     """
-    def __init__(self, formatfile, debug=False):
+    def __init__(self, year=None, formatfile="", debug=False):
         self.debug = debug
         self.parse_instr = []
         self.header_count = 0
-        self.headers = {}
+        self.headers = []
         self.descriptions = {}
 
+        if year:
+            self.year = year
+            self.formatfile = self.get_formatfile_name()
+        else:
+            self.formatfile = formatfile
+            self.year = 0
         # Load the network
-        self.read_formatfile(formatfile)
+        self.read_formatfile(self.formatfile)
 
     def __repr__(self):
         results = ""
         for instr in self.parse_instr:
             results += "Name:  %s, Size: %d\n" % (instr[0], instr[3] - instr[2] + 1)
         return results
+
+    # --------------------------------------
+    def get_formatfile_name(self):
+        return self.get_filename(formatfile_name)
+
+    # --------------------------------------
+    def get_datafile_name(self):
+        return self.get_filename(datafile_name)
+
+    # --------------------------------------
+    def get_filename(self, name_str):
+        """
+        Construct a filename from the base name, a year and
+        the local directory structure.
+        """
+        fname = name_str % (self.year%100, (self.year+1)%100)
+        this_dir, this_filename = os.path.split(__file__)
+        fname = os.path.join(this_dir, 'data', fname)
+        return fname
 
     # ==============================================================================
     # Read the Format file into a usable data structure
@@ -84,16 +116,21 @@ class NCESParser(object):
             print "=" * 80
             print "Reading Format File:  %s" % formatfile
             print "=" * 80
-        netlist = open(formatfile).read()
+        fh = open(formatfile, 'rb')
 
-        for line in longlines(netlist):
+        for line in fh:
             if re_definition.search(line):
-                self.add_instr(re_definition.search(line).groups())
+                col_name, type, loidx, hidix, size, description = re_definition.search(line).groups()
             elif re_sub_definition.search(line):
-                self.add_instr(re_sub_definition.search(line).groups())
+                col_name, type, loidx, hidix, size, description = re_sub_definition.search(line).groups()
+            elif re_alt_definition.search(line):
+                col_name, loidx, hiidx, size, type, description = re_alt_definition.search(line).groups()
+            elif re_alt_sub_definition.search(line):
+                col_name, loidx, hiidx, size, type, description = re_alt_sub_definition.search(line).groups()
             else:
                 print line
                 continue
+            self.add_instr(col_name, type, loidx, hiidx, size, description)
 
         if self.debug:
             print "=" * 80
@@ -105,8 +142,7 @@ class NCESParser(object):
             print "\n"
 
     # --------------------------------------
-    def add_instr(self, search_groups):
-        col_name, type, loidx, hiidx, size, description = search_groups
+    def add_instr(self, col_name, type, loidx, hiidx, size, description):
         if self.debug:
             print "Found Column:  %s - %s" % (col_name, size)
 
@@ -122,36 +158,62 @@ class NCESParser(object):
 
     # --------------------------------------
     def add_column(self, col_name, description):
-        self.headers[col_name] = self.header_count
+        self.headers.append(col_name)
         desc = description.strip().split('\t')[0]   # Filter out any Tab characters
         self.descriptions[col_name] = desc
         self.header_count += 1
 
     # --------------------------------------
-    def get_header(self):
+    def get_headers(self):
         return ",".join(self.header.keys())
 
     # --------------------------------------
     def get_idx(self, col_name):
-        return self.headers[col_name]
+        for i, name in enumerate(self.headers):
+            if name == col_name:
+                return i
+        raise Exception("Invalid Column Name:  %s" % col_name)
 
     # --------------------------------------
     def get_descriptions(self):
         return ",".join(self.descriptions)
 
     # --------------------------------------
-    def parse(self, line):
-        if self.debug:
-            print line
-
-        entry = {}
+    def parse_line(self, line):
+        entry = []
         for instr in self.parse_instr:
             sub_str = line[instr[2]:instr[3]]   # Python array slicing rules  low_idx : high_idx + 1
             if self.debug:
                 print sub_str
-            entry[instr[0]] = sub_str.strip()
+            entry.append(sub_str.strip())
         return entry
- 
+
+    # --------------------------------------
+    def parse(self, datafile=""):
+
+        if datafile:
+            fname = datafile
+        else:
+            fname = self.get_datafile_name()
+        fh = open(fname, 'rb')
+
+        # Pop the header line
+        line = fh.next()
+
+        self.schools = []
+        for line in fh:
+            self.schools.append(self.parse_line(line))
+
+        if self.debug:
+            print len(self.schools)
+        return self.schools
+
+    # --------------------------------------
+    def make_dict(self, school):
+        if self.debug:
+            print school
+        return dict(zip(self.headers, school))
+
 # *****************************************************************************
 # Unit Tests
 # *****************************************************************************
@@ -170,39 +232,59 @@ class TestBasicNetwork(unittest.TestCase):
 # Parse the command line options
 # -------------------------------------
 def main():
-    parser = argparse.ArgumentParser(description='Extracted Parasitic Capacitance netlist analyzer')
-    parser.add_argument('--formatfile', action='store', dest='formatfile', required=True,
-                        help='Input Netlist File')
-    parser.add_argument('--datafile', action='store', dest='datafile', required=True,
-                        help='Input Netlist File')
+    parser = argparse.ArgumentParser(description='NCES Data File Parser')
+    parser.add_argument('--year', action='store', dest='year', required=False, type=int,
+            help='NCES Data Year - Standard filenames assumed')
+    parser.add_argument('--formatfile', action='store', dest='formatfile', required=False,
+            help='NCES Data File Record Layout')
+    parser.add_argument('--datafile', action='store', dest='datafile', required=False,
+            help='NCES Data File')
     parser.add_argument('-debug', action='store_true',
-                       help='Print Debug Messages')
+            help='Print Debug Messages')
     args = parser.parse_args()
     # print args
 
     # -------------------------------------
     # Actually do the work we intend to do here
     # -------------------------------------
-    # NetworkX graph library routines
-    parse = NCESParser(args.formatfile, args.debug)
+    if args.year:
+        parse = NCESParser(year=args.year, debug=args.debug)
+    elif args.formatfile:
+        parse = NCESParser(formatfile=args.formatfile, debug=args.debug)
+    else:
+        raise Exception("Please select a year or an NCES Record Layout file name")
 
     print "=" * 80
     print parse
     print "=" * 80
-
-    fh = open(args.datafile, 'rb')
-    line = fh.next()
-    school = []
-    for line in fh:
-        school.append(parse.parse(line))
-    print len(school)
-    print school[0]
-    print school[22000]
-
-    print parse.headers
-    print parse.descriptions
-
     print parse.get_idx('GSLO')
+
+    if args.year:
+        schools = parse.parse()
+    elif args.formatfile:
+        schools = parse.parse(args.datafile)
+    else:
+        raise Exception("Please define a year when constructing or specify an NCES Datefile Layout file name")
+
+    print "=" * 80
+    print len(schools)
+
+    print "=" * 80
+    print schools[0]
+    print schools[22000]
+    import pprint
+    pprint.pprint(parse.make_dict(schools[22000]))
+
+    print "=" * 80
+    print "Headers"
+    print "=" * 80
+    print parse.headers
+
+    print "=" * 80
+    print "Descriptions"
+    print "=" * 80
+    pprint.pprint(parse.descriptions)
+
 
 
 # -------------------------------------
