c5c29891ea127aa288c12a511e878ed519887a6b
Robert Chapman <rchapman@kulikuli.net>
2013-05-15 00:54:34 -0700

    Converted into a dict with district names.

--- tuda.py
+++ tuda.py
@@ -3,27 +3,27 @@ Trial Urban Districts Assessment
 
 List of school districts assessed by NAEP as part of the TUDA program
 """
-tuda_dist = [
-    '3500060',  # Albuquerque Public School District
-    '1300120',  # Atlanta Public School
-    '4808940',  # Austin ISD
-    '2400090',  # Baltimore City School District
-    '2502790',  # Boston
-    '3702970',  # Charlotte-Mecklenburg Schools
-    '1709930',  # City of Chicago
-    '3904378',  # Cleveland Municipal
-    '4816230',  # Dallas ISD
-    '2612000',  # Detroit City
-    '1100030',  # DC Public Schools
-    '0614550',  # Fresno Unified
-    '1200870',  # Hillsborough (Florida)
-    '4823640',  # Houston ISD
-    '2102990',  # Jefferson County (KY)
-    '0622710',  # LA Unified
-    '1200390',  # Miami (Dade)
-    '5509600',  # Milwaukee School District
-    '3620580',  # New York (Geographic Districts Merged)
-    '0634320',  # San Diego Unified
-    '4218990'   # Philadelphia SD
-]
+tuda_dist = {
+    '3500060':  'Albuquerque',
+    '1300120':  'Atlanta',
+    '4808940':  'Austin ISD',
+    '2400090':  'Baltimore City',
+    '2502790':  'Boston',
+    '3702970':  'Charlotte-Mecklenburg',
+    '1709930':  'City of Chicago',
+    '3904378':  'Cleveland Municipal',
+    '4816230':  'Dallas ISD',
+    '2612000':  'Detroit City',
+    '1100030':  'DC Public Schools',
+    '0614550':  'Fresno Unified',
+    '1200870':  'Hillsborough (FL)',
+    '4823640':  'Houston ISD',
+    '2102990':  'Jefferson County (KY)',
+    '0622710':  'LA Unified',
+    '1200390':  'Miami (Dade)',
+    '5509600':  'Milwaukee',
+    '3620580':  'New York',
+    '0634320':  'San Diego Unified',
+    '4218990':  'Philadelphia'
+}
 
