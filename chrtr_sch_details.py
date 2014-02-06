03b3e9d5831b3d14a8c6e324fb697769015f0857
Robert Chapman <rchapman@kulikuli.net>
2014-02-05 20:10:24 -0800

    Added support for regular school and magnet summaries.  Included district totals as well.

--- chrtr_sch_details.py
+++ chrtr_sch_details.py
@@ -53,8 +53,11 @@ def main(argv):
     schools = nces.parse(make_dict=True)
 
     wb = Workbook()
-    ws = wb.add_sheet("Charter Summary")
-    row_offset = 1
+    sheets = []
+    sheets.append(wb.add_sheet("School Summary"))
+    sheets.append(wb.add_sheet("Charter Summary"))
+    sheets.append(wb.add_sheet("Magnet Summary"))
+    row_offset = [2, 2, 2]
 
     # --------------------------------------
     # Create the column labels
@@ -69,7 +72,9 @@ def main(argv):
         "Hisp Enrollment",
         "Hisp Proportion",
         "Minority Enrollment",
-        "Minority Proportion"
+        "Minority Proportion",
+        "Other Enrollment",
+        "Other Proportion"
     ]
 
     indexes = [
@@ -85,37 +90,68 @@ def main(argv):
     # Common Data Across minorities
     # e.g. Total Students in a district
     for header in headers:
-        ws.write(0, col_offset, header)
+        for ws in sheets:
+            ws.write(0, col_offset, header)
         col_offset += 1
 
     # --------------------------------------
     # Now fill in the static data data
     # --------------------------------------
+    dist_total = 0.0
+    dist_white = 0.0
+    dist_black = 0.0
+    dist_hisp = 0.0
     for school in schools:
         if (
             school['LEAID'] == leaid and
-            school['STATUS'] == '1' and
-            school['CHARTR'] == '1'
+            school['STATUS'] == '1'
         ):
-            print school
+            # print school
+            if school['CHARTR'] == '1':
+                idx = 1
+            elif school['MAGNET'] == '1':
+                idx = 2
+            else:
+                idx = 0
 
             name = school['SCHNAM']
             total = float(school['MEMBER'])
             white = float(school['WHITE'])
             black = float(school['BLACK'])
             hisp = float(school['HISP'])
+            dist_total += total
+            dist_white += white
+            dist_black += black
+            dist_hisp += hisp
 
-            ws.write(row_offset, 0, name)
-            ws.write(row_offset, 1, total)
-            ws.write(row_offset, 2, white)
-            ws.write(row_offset, 3, white/total)
-            ws.write(row_offset, 4, black)
-            ws.write(row_offset, 5, black/total)
-            ws.write(row_offset, 6, hisp)
-            ws.write(row_offset, 7, hisp/total)
-            ws.write(row_offset, 8, black+hisp)
-            ws.write(row_offset, 9, (black+hisp)/total)
-            row_offset += 1
+            sheets[idx].write(row_offset[idx], 0, name)
+            sheets[idx].write(row_offset[idx], 1, total)
+            sheets[idx].write(row_offset[idx], 2, white)
+            sheets[idx].write(row_offset[idx], 3, white/total)
+            sheets[idx].write(row_offset[idx], 4, black)
+            sheets[idx].write(row_offset[idx], 5, black/total)
+            sheets[idx].write(row_offset[idx], 6, hisp)
+            sheets[idx].write(row_offset[idx], 7, hisp/total)
+            sheets[idx].write(row_offset[idx], 8, black+hisp)
+            sheets[idx].write(row_offset[idx], 9, (black+hisp)/total)
+            sheets[idx].write(row_offset[idx], 10, total-white-black-hisp)
+            sheets[idx].write(row_offset[idx], 11, (total-white-black-hisp)/total)
+            row_offset[idx] += 1
+
+    print "District Summary"
+    for idx in range(3):
+        sheets[idx].write(1, 0, "District Totals")
+        sheets[idx].write(1, 1, dist_total)
+        sheets[idx].write(1, 2, dist_white)
+        sheets[idx].write(1, 3, dist_white/dist_total)
+        sheets[idx].write(1, 4, dist_black)
+        sheets[idx].write(1, 5, dist_black/dist_total)
+        sheets[idx].write(1, 6, dist_hisp)
+        sheets[idx].write(1, 7, dist_hisp/dist_total)
+        sheets[idx].write(1, 8, dist_black+dist_hisp)
+        sheets[idx].write(1, 9, (dist_black+dist_hisp)/dist_total)
+        sheets[idx].write(1, 10, dist_total-dist_white-dist_black-dist_hisp)
+        sheets[idx].write(1, 11, (dist_total-dist_white-dist_black-dist_hisp)/dist_total)
 
     print "Generating Report"
     wb.save(args.outfile)
